73618daa011e9729f00501d8c795e935435b7b02
MacBook Pro <hello@MacBooks-MacBook-Pro.local>
2024-02-28 21:39:19 +0600

    class-4-5

---
+++ script/class-2-3.js
@@ -0,0 +1,7 @@
+// <!-- day - 2 & 3 -->
+    // var t = "tbj"
+    // alert("This Is For Alert")
+    // document.write(" <H3>This is For write within HTML </H3> <br>")
+    // document.write("I Love to Practise Javascript")
+    // console.log("This Is for Console print") ;
+    // console.log(typeof(t))
---
+++ script/class-4-5.js
@@ -0,0 +1,16 @@
+// // <!-- day - 4-5 -->
+
+
+//         var namee = "tuhin " ;
+//         var age = 21 ;
+//         document.write(namee) ;
+//         document.write(age) ;
+//         namee = "ahammed " ;
+//         age = 20 ;
+//         document.write(namee);
+//         document.write(age)
+//     var country , hobby , id ;
+//     country = "bangladesh"
+//     hobby = "playing"
+//     id = 243
+//     document.write(country, hobby , id)
