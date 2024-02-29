e9e9f7575088e404716a327289e3e8afd879d7c8
MacBook Pro <hello@MacBooks-MacBook-Pro.local>
2024-03-01 01:26:00 +0600

    class-9-10-s10 (Operator)

--- script/class-7-8.js
+++ script/class-7-8.js
@@ -1,25 +1,25 @@
-// var userInput = prompt("Enter Your Name & Check your Name Length")
-// var userNameLength = userInput.length
-// document.write("Your Name Langth is " + userNameLength)
-//  var username = "bangladesh"
-//  document.write(username.charAt(2))
-var userName = prompt("Enter Your Name")
-var userNameChar = prompt("Enter Your ChartIndex")
-if (!userName){
-    document.write("Enter Your Name")
-}
-else if (userName.length < 3){
-    document.write("Enter Minimum 3 String")
-}
-else if(!userNameChar){
-    document.write("Enter your needed CharIndex")
-}
-else if ( userNameChar.length > userName.length){
-    document.write("Your CharIndex number  is gretter then Your Name, Please Enter Within " + userName.length)
-}
-else{
-    document.write( "Your Needed Charecter : " + userName.toUpperCase().charAt(userNameChar))
-}
+// // var userInput = prompt("Enter Your Name & Check your Name Length")
+// // var userNameLength = userInput.length
+// // document.write("Your Name Langth is " + userNameLength)
+// //  var username = "bangladesh"
+// //  document.write(username.charAt(2))
+// var userName = prompt("Enter Your Name")
+// var userNameChar = prompt("Enter Your ChartIndex")
+// if (!userName){
+//     document.write("Enter Your Name")
+// }
+// else if (userName.length < 3){
+//     document.write("Enter Minimum 3 String")
+// }
+// else if(!userNameChar){
+//     document.write("Enter your needed CharIndex")
+// }
+// else if ( userNameChar.length < userName.length){
+//     document.write("Your CharIndex number  is gretter then Your Name, Please Enter Within " + userName.length)
+// }
+// else{
+//     document.write( "Your Needed Charecter : " + userName.toUpperCase().charAt(userNameChar))
+// }
 
-document.write("</br>" + "Your Full Input : " + userName.concat(userNameChar))
-document.write("</br>" + "0 to Your User Charecter : " + userName.toUpperCase().slice(0 , userNameChar))
+// document.write("</br>" + "Your Full Input : " + userName.concat(userNameChar))
+// document.write("</br>" + "0 to Your User Charecter : " + userName.toUpperCase().slice(0 , userNameChar))
---
+++ script/class-9-10.js
@@ -0,0 +1,29 @@
+// JavaScript all operator 
+// Threre are three types of Operator in Javascript
+
+// Arithmetic Operators : " + , - , ++ , -- , * , ** , / , %  "
+// Assignment Operators : " = , += , -= , *= , /= , %= , **=  "
+// Comparison Operators : " == , === , != , !== , > , < , >= ,<= , ?  "
+// Logical Operators     : " && , || , ! "
+
+var sum1 = prompt("Enter First Value")
+var sum2 = prompt("Enter Second Value")
+
+sum1 = parseInt(sum1 , 10)
+sum2 = parseInt(sum2 , 10)
+var sum , sub , mul , div , mod;
+
+sum = sum1 + sum2
+document.write("Your Adision Value is :" + sum1 + "+" + sum2  + " = " + sum + "</br>")
+
+sub = sum1 - sum2 
+document.write("Your Subtraction Value Is :"+ sum1 + "-" + sum2  + " = " + sub + "</br>")
+
+mul = sum1 * sum2
+document.write("Your Multiplication Value is :" + sum1 + "*" + sum2  + " = " + mul + "</br>")
+
+div = sum1 / sum2
+document.write("Your Division Value is :" + sum1 + "/" + sum2  + " = " + div + "</br>")
+
+mod = sum1 % sum2
+document.write("Your Modulus Value is :" + sum1 + "%" + sum2  + " = " + mod + "</br>")
