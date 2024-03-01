4b12aac2a125a90be6a495e5a76cf28b545afe41
MacBook Pro <hello@MacBooks-MacBook-Pro.local>
2024-03-01 18:48:28 +0600

    class-12-15 Grade Program

---
+++ script/class-11-15.js
@@ -0,0 +1,26 @@
+var result = prompt("Enter Your Result")
+if(result > 100){
+    alert("Enter Your Value Under 100")
+}
+else if (result >=80 ){
+    document.write("You Gate A+")
+}
+else if (result >= 70){
+    document.write("You Gate A")
+}
+else if (result >= 60){
+    document.write("You Gate A-")
+}
+else if (result >= 50){
+    document.write("You Gate B")
+}
+else if (result >= 40){
+    document.write("You Gate D")
+}
+else if (result >= 33){
+    document.write("You Gate D")
+}
+
+else{
+    document.write("You are Failed")
+}
