e9a72565b4efbcf203699fa8b2f6dc7bc8d3311b
MacBook Pro <hello@MacBooks-MacBook-Pro.local>
2024-03-05 17:17:43 +0600

    class-18-22-s13-s14 For , while , do while Loop

---
+++ script/class-18-s13.js
@@ -0,0 +1,19 @@
+// // for loop 
+
+// for ( var x = 1 ; x <= 100 ; x= x+2){
+//     document.write(" " + x)
+// }
+// document.write("</br>")
+
+// for ( var a = 100 ; a >= 1 ; a= a-1){
+//     document.write(" " + a)
+// }
+
+// document.write("</br> </br>")
+
+// var sum = 0 ;
+// for (var n = 1 ; n <= 100 ;  n++) {
+//     // document.write(n)
+//     sum = sum + n
+// }
+// document.write(sum)
