e3b91dd7c3f55e5930eb141ef1d12979ef133b42
tuhinAhammed <tuhinahamed2030@gmail.com>
2024-03-06 21:33:44 +0600

    class-25-30-s15

---
+++ script/class-26-s15.js
@@ -0,0 +1,44 @@
+// Array Push & pop method
+
+// var names = ["tuhin" , "rahim" , "jon" , "pop" , "guru"]
+// document.write(names.length)
+// document.write(names)
+
+// // names.pop()
+// names.push()
+// console.log(names)
+
+
+// ********** class-27 -    loop with array ********* //
+//  var number = [10 , 20 , 30 , 40 , 50]
+//  sum = 0
+// for (var i = 0 ; i < 5 ; i++){
+//     console.log(number[i]);
+//     sum = sum + number[i]
+// }
+// console.log(sum);
+
+// ******* array value with prompt()
+
+// var num = new Array()
+// sum = 0
+// for(var i = 0 ; i < 5 ; i++){
+//     num[i] = parseInt(prompt("Enter Your Number"))
+//     sum = sum +  num[i]
+// }
+// console.log(num);
+// console.log(sum);
+
+// ******* array value with prompt()
+ var names = ["tuhin" , "rakib " , "loya" , "luna" ]
+// ***** shift method
+//  names.shift()
+//  console.log(names);
+
+// unshift() method
+// names.unshift("tuhins")
+// console.log(names);
+
+//****** */ splice method
+names.splice(1 , 2 , "tuan" ,"efa")
+console.log(names);
