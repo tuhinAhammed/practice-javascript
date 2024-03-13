9373ed6404a853f027ad6df3f71e74dcd7d101cc
tuhinAhammed <tuhinahamed2030@gmail.com>
2024-03-14 00:52:49 +0600

    class-37-OnClick

---
+++ class-37-onClick/script.js
@@ -0,0 +1,8 @@
+var imageList = document.getElementById("changeImage")
+console.log(imageList);
+function artcel(){
+    imageList.src = "../class-37-onClick/images/3.jpg"
+}
+function ashes(){
+    imageList.src = "../class-37-onClick/images/1.jpg"
+}
