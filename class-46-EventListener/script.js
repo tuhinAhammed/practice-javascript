59fbd74e98cd23dabdc0d7561eafae7f5deb49e2
tuhinAhammed <tuhinahamed2030@gmail.com>
2024-04-04 16:49:50 +0600

    addEventListener - class-51-55-s-22

--- class-46-EventListener/script.js
+++ class-46-EventListener/script.js
@@ -57,6 +57,7 @@ var passwordInput = form.querySelector("label .password")
 var submitInput = form.querySelector("label .submit")
 var formOutput = document.querySelector(".formOutput")
 form.addEventListener("submit" , (e) => {
+    console.log(submitInput);
     e.preventDefault()
     console.log(form);
     var formData = {
@@ -69,7 +70,16 @@ form.addEventListener("submit" , (e) => {
     emailInput.value = "" ,
     passwordInput.value = ""
 })
+submitInput.addEventListener("focusin" , (e) => {
+    console.log(e.target)
+    submitInput.classList.add("submitAnim")
+    setTimeout(() => {
+    submitInput.classList.remove("submitAnim")
 
+    }, [1000])
+
+
+})
 // *********** Scroll  - Event ***********//
 var output = document.querySelector(".output")
 window.addEventListener("scroll" , (e) => {
@@ -84,5 +94,141 @@ var toggle = document.querySelector("details")
 toggle.addEventListener("toggle" , (e) => {
     console.log(e.target.open);
     output.innerHTML = "Event Output : " + e.type  + e.target.open;
+})
+
+// *********** Mouse -- dblclick  - Event ***********//
+
+var mouseEvent = document.querySelector(".mouseEvent")
+var mouseOutput = document.querySelector(".mouseOutput")
+console.log(mouseEvent);
+
+// *********** mouseover  - Event ***********//
+
+mouseEvent.addEventListener("mouseenter" , (e) => {
+    mouseOutput.innerHTML = "Event Output : " + e.type
+    mouseEvent.classList.add("mouseEventAnim")
+})
+
+// *********** mouseleave  - Event ***********//
+
+mouseEvent.addEventListener("mouseleave" , (e) => {
+    mouseOutput.innerHTML = "Event Output : " + e.type
+    mouseEvent.classList.remove("mouseEventAnim")
+})
+
+// *********** mouseover  - Event ***********//
+
+mouseEvent.addEventListener("mouseover" , (e) => {
+    mouseOutput.innerHTML = "Event Output : " + e.type
+    mouseEvent.classList.add("mouseEventAnim")
+})
+
+// *********** mousemove  - Event ***********//
+// mouseEvent.addEventListener("mousemove" , (e) => {
+//     mouseOutput.innerHTML = "Event Output : " + e.type
+//     mouseEvent.classList.add("mouseEventAnim")
+// })
+
+// *********** mousedown  - Event ***********//
+    mouseEvent.addEventListener("mousedown" , (e) => {
+        mouseOutput.innerHTML = "Event Output : " + e.type ;
+        mouseEvent.classList.add("mouseEventAnim")
+        console.log(e.type);
+    })
+
+
+// *********** mouseup  - Event ***********//
+
+mouseEvent.addEventListener("mouseup" , (e) => {
+    mouseOutput.innerHTML = "Event Output : " + e.type
+    mouseEvent.classList.remove("mouseEventAnim")
+    console.log(e.type);
+})
+
+// *********** Focus  - Event ***********//
+
+var focusEvent = document.querySelector(".focusEvent")
+var focusOutput = document.querySelector(".focusOutput")
+// *********** Focus  - Event ***********//
+focusEvent.addEventListener("focus" , (e) => {
+    focusEvent.classList.add("focusEventAnim")
+    focusOutput.innerHTML ="Event Output : " + e.type
+    console.log(e.type);
+})
+
+// *********** Focusin  - Event ***********//
+
+focusEvent.addEventListener("focusin" , (e) => {
+    focusEvent.classList.add("focusEventAnim")
+    focusOutput.innerHTML ="Event Output : " + e.type
+    console.log(e.type);
+})
+
+// *********** Focusout  - Event ***********//
+
+focusEvent.addEventListener("focusout" , (e) => {
+    focusEvent.classList.remove("focusEventAnim")
+    focusOutput.innerHTML ="Event Output : " + e.type
+    console.log(e.type);
+})
+// *********** blur  - Event ***********//
+// focusEvent.addEventListener("blur" , (e) => {
+//     focusEvent.style.background = "tomato"
+//     focusOutput.innerHTML ="Event Output : " + e.type
+//     console.log(e.type);
+// })
+
+
+// *********** clickBoard  - Event ***********//
+// copy , cut , paste
+var clickboardEvent = document.querySelector(".clickboardEvent")
+var clickboardOutput = document.querySelector(".clickboardOutput")
+
+// *********** copy  - Event ***********//
+clickboardEvent.addEventListener("copy" , (e) => {
+    clickboardEvent.classList.add("clickboardEventAnim")
+    clickboardOutput.innerHTML = "you Are " + e.type + " text from here"
+})
+clickboardEvent.addEventListener("cut" , (e) => {
+    clickboardEvent.classList.add("clickboardEventAnim")
+    clickboardOutput.innerHTML = "you Are " + e.type + " text from here" 
+})
+clickboardEvent.addEventListener("paste" , (e) => {
+    clickboardEvent.classList.add("clickboardEventAnim")
+    clickboardOutput.innerHTML = "you Are " + e.type + " text from here"
+})
+
+// *********** Drag  - Event ***********//
+// dragstart , dragend , drag , dragenter , dragleave , dragover , drop
+
+var dragHere = document.querySelector(".dragHere")
+var dragText = document.querySelector(".dragText")
+console.log(dragText);
+
+dragText.addEventListener("dragstart" , (e) =>{
+e.dataTransfer.setData("Text" , e.target.classList)
+// console.log("start");
+}) 
+dragHere.addEventListener("dragover" , (e) =>{
+    // console.log(e.target);
+    // console.log("ok");
+    e.preventDefault()
+    // dragHere.classList.add("dragHereAnim")
+
+})
+dragHere.addEventListener("drop" , (e) => {
+        dragHere.classList.add("dragHereAnim")
+        dragHere.textContent = ""
+        var data = e.dataTransfer.getData("Text")
+        dragHere.appendChild(document.querySelector(".dragText"))
+        console.log("drop");
+})
+
+dragHere.addEventListener("dragenter" , (e) => {
+    dragHere.classList.add("dragHereAnim")
+
+})
+dragHere.addEventListener("dragleave" , (e) => {
+    dragHere.classList.remove("dragHereAnim")
 
 })
