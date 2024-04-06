dc018a21180bf144ec5495f66d61288e83b43013
tuhinAhammed <tuhinahamed2030@gmail.com>
2024-04-06 12:50:18 +0600

    Day 60-70 - Es6

--- class-56-60-BOM/script.js
+++ class-56-60-BOM/script.js
@@ -11,45 +11,59 @@
 // }
 
 
-function timeZone() {
-    var time = document.querySelector(".time")
-    var date = new Date()
-    var hour = date.getHours()
-    var munite = date.getMinutes()
-    var second = date.getSeconds()
-
-    munite = formatTime(munite)
-    second = formatTime(second)
-    var allTime = hour + ":" + munite + ":" + second
-    time.innerHTML = allTime 
-}
-setInterval(timeZone , 1000)
-function formatTime(value){
-    if(value < 10){
-        value = "0" + value 
+try{
+    demo()
+    function timeZone() {
+        var time = document.querySelector(".time")
+        var date = new Date()
+        var hour = date.getHours()
+        var munite = date.getMinutes()
+        var second = date.getSeconds()
+    
+        munite = formatTime(munite)
+        second = formatTime(second)
+        var allTime = hour + ":" + munite + ":" + second
+        time.innerHTML = allTime 
     }
-    console.log(value);
-    return value
 }
+catch(err){
+    confirm("error found")
+    console.error(err.message);
+    console.error(err.name);
 
-function promptWork () {
-    var demo = prompt("Give Your Name")
-    var text = document.createElement("p")
-    console.log(demo);
-    if(demo == null || demo == ""){
-        var promptDiv = document.querySelector(".prompt")
+}
+finally{
+    console.log("ok");
+}
+    setInterval(timeZone , 1000)
+    function formatTime(value){
+        if(value < 10){
+            value = "0" + value 
+        }
+        // console.log(value);
+        return value
+    }
+    
+    function promptWork () {
+        var demo = prompt("Give Your Name")
         var text = document.createElement("p")
-        promptDiv.appendChild(text)
-        text.textContent = "Not Founded"
-    }
-    else{
-        console.log("ok");
+        console.log(demo);
+        if(demo == null || demo == ""){
             var promptDiv = document.querySelector(".prompt")
             var text = document.createElement("p")
             promptDiv.appendChild(text)
-            text.textContent = demo
+            text.textContent = "Not Founded"
+        }
+        else{
+            console.log("ok");
+                var promptDiv = document.querySelector(".prompt")
+                var text = document.createElement("p")
+                promptDiv.appendChild(text)
+                text.textContent = demo
+        }
     }
-}
+
+
 
 // promptWork()
 // setInterval(promptWork , [2000]);
